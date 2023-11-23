0e990cde8bc26328e3963e713ede0e2c90cf22a3
andrei-lucian <lucianandrei12@gmail.com>
2023-11-23 14:57:59 +0100

    get all legal moves and gitignore some stuff

--- team13_A0/sudokuai.py
+++ team13_A0/sudokuai.py
@@ -23,12 +23,60 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
         def possible(i, j, value):
             return game_state.board.get(i, j) == SudokuBoard.empty \
                    and not TabooMove(i, j, value) in game_state.taboo_moves
+        
+        def legal_moves():      
+            all_moves = [Move(i, j, value) for i in range(N) for j in range(N)
+                        for value in range(1, N+1) if possible(i, j, value)]
 
-        all_moves = [Move(i, j, value) for i in range(N) for j in range(N)
-                     for value in range(1, N+1) if possible(i, j, value)]
-        move = random.choice(all_moves)
+            all_legal_moves = []
+
+            for move in all_moves:
+                legal = True
+
+                # Check if value is repeated in column
+                for column in range(N):
+                    if game_state.board.get(column, move.j) == move.value:
+                        legal = False
+                        break
+
+                if legal:
+                    # Check if value is repeated in row
+                    for row in range(N):
+                        if game_state.board.get(move.i, row) == move.value:
+                            legal = False
+                            break
+                else:
+                    continue
+
+                if legal:
+                    # Find the coordinates of the upper left corner of the block in which the current possible move is positioned
+                    block_row_coordinate = move.i // game_state.board.m * game_state.board.m
+                    block_column_coordinate = move.j // game_state.board.n * game_state.board.n
+
+                    print("row: ", block_row_coordinate, "column: ", block_column_coordinate)
+
+                    # Loop through rows and columns in block that contains current move
+                    for row in range(block_row_coordinate, block_row_coordinate + game_state.board.m): # Loop from current coordinate to size of block in row direction (m)
+                        for column in range(block_column_coordinate, block_column_coordinate + game_state.board.n): # Loop from current coordinate to size of block in column direction (n)
+                            if game_state.board.get(row, column) == move.value:
+                                legal = False
+                                break
+                else:
+                    continue
+
+                # If all checks are passed add the move to the list of legal moves
+                if legal:
+                    all_legal_moves.append(move)
+                else:
+                    continue
+
+            return all_legal_moves
+
+        
+        all_legal_moves = legal_moves()
+        move = random.choice(all_legal_moves)
         self.propose_move(move)
         while True:
             time.sleep(0.2)
-            self.propose_move(random.choice(all_moves))
+            self.propose_move(random.choice(all_legal_moves))
 
